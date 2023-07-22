4ccb54875e9e957692ab983f3364a7aa04558b51
合肥海图微电子有限公司 <49272093+haitusense@users.noreply.github.com>
2023-07-22 13:25:08 +0900

    update

--- sample/script.r
+++ sample/script.r
@@ -6,10 +6,15 @@ library(XML)
 dst <- list(id = "id9_out", value = "form R")
 rsquid::named_pipe("NamedPipe", toJSON(dst))
 
+pdf(NULL) # Rplots.pdf の作成を抑制
 p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
 p
-svg <- saveXML(grid.export(NULL, addClasses = TRUE, prefix = "ggridsvg")$svg)
-dst <- list(id = "plot_svg", value = svg)
+svg <- grid.export(NULL, addClasses = TRUE, prefix = "gridsvg")$svg
+svg <- XML::removeAttributes(svg, "width")
+svg <- XML::addAttributes(svg, "height" = "100%")
+svg <- XML::removeChildren(svg, "metadata")
+
+dst <- list(id = "plot_svg", value = saveXML(svg))
 rsquid::named_pipe("NamedPipe", toJSON(dst))
 
 Sys.sleep(3)
